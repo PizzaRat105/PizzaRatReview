e5bbde15110cf9acd8c0725d9cef7fa0f0fb0ad6
PizzaRat105 <142154299+PizzaRat105@users.noreply.github.com>
2023-10-16 15:32:14 +0900

    Update button_toggles.js

--- button_toggles.js
+++ button_toggles.js
@@ -49,92 +49,6 @@ document.addEventListener('keydown', event => {
     LootBox.style.display = 'none';
   }
 });
-/*Provisions*/
-const ProvisionerBtn = document.getElementById('provisioner')
-const ProvisionerBox = document.getElementById('provisionbox')
-const RuinsNavigate = document.getElementById('ruins_nav')
-const WealdNavigate = document.getElementById('weald_nav')
-const WarrensNavigate = document.getElementById('warrens_nav')
-const CoveNavigate = document.getElementById('cove_nav')
-const CourtyardNavigate = document.getElementById('courtyard_nav')
-const FarmsteadNavigate = document.getElementById('farmstead_nav')
-
-const RuinsArea = document.getElementById('ruins_area')
-const WealdArea = document.getElementById('weald_area')
-const WarrensArea = document.getElementById('warrens_area')
-const CoveArea = document.getElementById('cove_area')
-const CourtyardArea = document.getElementById('courtyard_area')
-const FarmsteadArea = document.getElementById('farmstead_area')
-
-
-ProvisionerBtn.addEventListener('click' , function() {
-  PlayPageOpn()
-  ProvisionerBox.style.display = 'block'
-})
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape' && ProvisionerBox.style.display === 'block') {
-    CloseAudio.currentTime = 0;
-    CloseAudio.play();
-    ProvisionerBox.style.display = 'none';
-  }
-});
-
-
-RuinsNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'flex'
-  WealdArea.style.display = 'none'
-  WarrensArea.style.display = 'none'
-  CoveArea.style.display = 'none'
-  CourtyardArea.style.display = 'none'
-  FarmsteadArea.style.display = 'none'
-})
-WealdNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'none'
-  WealdArea.style.display = 'flex'
-  WarrensArea.style.display = 'none'
-  CoveArea.style.display = 'none'
-  CourtyardArea.style.display = 'none'
-  FarmsteadArea.style.display = 'none'
-})
-WarrensNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'none'
-  WealdArea.style.display = 'none'
-  WarrensArea.style.display = 'flex'
-  CoveArea.style.display = 'none'
-  CourtyardArea.style.display = 'none'
-  FarmsteadArea.style.display = 'none'
-})
-CoveNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'none'
-  WealdArea.style.display = 'none'
-  WarrensArea.style.display = 'none'
-  CoveArea.style.display = 'flex'
-  CourtyardArea.style.display = 'none'
-  FarmsteadArea.style.display = 'none'
-})
-CourtyardNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'none'
-  WealdArea.style.display = 'none'
-  WarrensArea.style.display = 'none'
-  CoveArea.style.display = 'none'
-  CourtyardArea.style.display = 'flex'
-  FarmsteadArea.style.display = 'none'
-})
-FarmsteadNavigate.addEventListener('click', function() {
-  PlayPageOpn()
-  RuinsArea.style.display = 'none'
-  WealdArea.style.display = 'none'
-  WarrensArea.style.display = 'none'
-  CoveArea.style.display = 'none'
-  CourtyardArea.style.display = 'none'
-  FarmsteadArea.style.display = 'flex'
-})
-
 
 
 /*Districts*/
@@ -303,7 +217,6 @@ SettingIcon1.addEventListener('mouseleave', function() {SettingOverlay.style.dis
 SettingIcon2.addEventListener('mouseover' , function() {SettingOverlay.style.display = 'block'})
 SettingIcon2.addEventListener('mouseleave', function() {SettingOverlay.style.display = 'none'; });
 
-const CloseProvisions = document.getElementById('food_close')
 const CloseLoot = document.getElementById('lootclose');
 const DiseaseCloseBtn = document.getElementById("disease_close_btn"); 
 const CloseAudio = document.getElementById('page_close');
@@ -314,11 +227,7 @@ DiseaseCloseBtn.addEventListener('click', function() {
   CloseAudio.play();
   DiseaseBox.style.display = 'none';
 });
-CloseProvisions.addEventListener('click', function() {
-  CloseAudio.currentTime = 0;
-  CloseAudio.play();
-  ProvisionerBox.style.display = 'none'
-})
+
 CloseLoot.addEventListener('click', function() {
   CloseAudio.currentTime = 0;
   CloseAudio.play();
