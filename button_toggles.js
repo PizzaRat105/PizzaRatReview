62d78e5a19a749256a78cc0867b86cd035da7cd5
PizzaRat105 <142154299+PizzaRat105@users.noreply.github.com>
2024-02-07 11:44:23 +0900

    Update button_toggles.js

--- button_toggles.js
+++ button_toggles.js
@@ -2,65 +2,27 @@
 const BtnSelectAudio = document.getElementById('btn_press');
 BtnSelectAudio.volume = 0.4;
 const OpenAudio = document.getElementById('page_open');
-OpenAudio.volume = 0.4;
 
 const CloseAudio = document.getElementById('page_close');
-CloseAudio.volume = 0.5;
 
 function PlayPageOpn() {
   OpenAudio.currentTime = 0;
+  OpenAudio.volume = 0.4;
   OpenAudio.play();
 }
 
 function PlayPageClose() {
   CloseAudio.currentTime = 0;
+  CloseAudio.volume = 0.4;
   CloseAudio.play();
 }
 
 function PlayBtnPress() {
-  BtnSelectAudio.currentTime = 0; 
+  BtnSelectAudio.currentTime = 0;
+  BtnSelectAudio.volume = 0.4; 
   BtnSelectAudio.play();
 }
-/*Disease Box*/
-const DiseaseBox = document.getElementById("diseasebox");
-const DiseaseBtn = document.getElementById("diseasebtn");
 
-DiseaseBtn.addEventListener('click' , function() {
-  PlayPageOpn();
-  DiseaseBox.style.display = 'block';
-});
-
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape' && DiseaseBox.style.display === 'block') {
-    PlayPageClose()
-
-    DiseaseBox.style.display = 'none';
-  }
-});
-
-/*Resolve Box*/
-const ResolveBox = document.getElementById("resolve_box");
-const AfflictionBtn = document.getElementById("affliction_btn");
-const VirtueBtn = document.getElementById("virtue_btn");
-
-AfflictionBtn.addEventListener('click' , function() {
-  PlayPageOpn();
-  ResolveBox.style.display = 'block';
-});
-
-VirtueBtn.addEventListener('click' , function() {
-  PlayPageOpn();
-  ResolveBox.style.display = 'block';
-});
-
-
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape' && ResolveBox.style.display === 'block') {
-    PlayPageClose()
-
-    ResolveBox.style.display = 'none';
-  }
-})
 
 /*Torch Box*/
 const TorchBox = document.getElementById("torchlight_box");
@@ -98,7 +60,6 @@ document.addEventListener('keydown', event => {
     LootBox.style.display = 'none';
   }
 });
-
 
 /*Districts*/
 const DistrictOpen = document.getElementById('district_btn_open')
@@ -153,98 +114,6 @@ DistrictOpen.addEventListener('mouseleave', function() {DistrictOverlay.style.di
 DistrictClosed.addEventListener('mouseover' , function() {DistrictOverlay.style.display = 'block'})
 DistrictClosed.addEventListener('mouseleave', function() {DistrictOverlay.style.display = 'none'; });
 
-/*Town Event */
-const EventOpen = document.getElementById('town_event_open')
-const EventClosed = document.getElementById('town_event_closed')
-const EventOverlay = document.getElementById('event_overlay')
-const EventBox = document.getElementById('town_event_box');
-const ClosedEvent = document.getElementById('town_event_close');
-
-const TownEventGeneralBanner = document.getElementById('general_event_banner');
-const TownEventQuestBanner = document.getElementById('quest_event_banner');
-const TownEventFacilityBanner = document.getElementById('facility_event_banner');
-const TownEventHeroBanner = document.getElementById('hero_event_banner');
-const TownEventDLCBanner = document.getElementById('dlc_event_banner');
-
-EventOpen.addEventListener('click' , function() {
-  PlayBtnPress()
-  EventClosed.style.display = 'block'
-  EventOpen.style.display = 'none'
-  EventBox.style.display = 'none'
-})
-EventClosed.addEventListener('click' , function() {
-  PlayBtnPress()
-  EventClosed.style.display = 'none'
-  EventOpen.style.display = 'block'
-  EventBox.style.display = 'block'
-  const eventInfoContainers = document.querySelectorAll('.event_info_container');
-
-    eventInfoContainers.forEach(function(container) {
-        container.style.display = 'none';
-    });
-})
-
-TownEventGeneralBanner.addEventListener('click' , function() {
- 
-const eventInfoContainers = document.querySelectorAll('.event_info_container');
-eventInfoContainers.forEach(function(container) {
-    container.style.display = 'none';
-});
-})
-TownEventQuestBanner.addEventListener('click' , function() {
- 
-  const eventInfoContainers = document.querySelectorAll('.event_info_container');
-  eventInfoContainers.forEach(function(container) {
-      container.style.display = 'none';
-  });
-  })
-
-TownEventFacilityBanner.addEventListener('click' , function() {
- 
-const eventInfoContainers = document.querySelectorAll('.event_info_container');
-eventInfoContainers.forEach(function(container) {
-    container.style.display = 'none';
-});
-})
-
-TownEventHeroBanner.addEventListener('click' , function() {
- 
-const eventInfoContainers = document.querySelectorAll('.event_info_container');
-eventInfoContainers.forEach(function(container) {
-    container.style.display = 'none';
-});
-})
-
-TownEventDLCBanner.addEventListener('click' , function() {
- 
-const eventInfoContainers = document.querySelectorAll('.event_info_container');
-eventInfoContainers.forEach(function(container) {
-    container.style.display = 'none';
-});
-})
-
-
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape' && EventBox.style.display === 'block') {
-    PlayBtnPress()
-    EventClosed.style.display = 'block'
-    EventOpen.style.display = 'none'
-    EventBox.style.display = 'none';
-  }
-});
-
-ClosedEvent.addEventListener('click' , function() {
-  PlayPageClose()
-
-  EventClosed.style.display = 'block'
-  EventOpen.style.display = 'none'
-  EventBox.style.display = 'none'
-})
-
-EventOpen.addEventListener('mouseover' , function() {EventOverlay.style.display = 'block'})
-EventOpen.addEventListener('mouseleave', function() {EventOverlay.style.display = 'none'; });
-EventClosed.addEventListener('mouseover' , function() {EventOverlay.style.display = 'block'})
-EventClosed.addEventListener('mouseleave', function() {EventOverlay.style.display = 'none'; });
 
 
 /*Activity Section */
@@ -362,25 +231,15 @@ SettingIcon2.addEventListener('mouseover' , function() {SettingOverlay.style.dis
 SettingIcon2.addEventListener('mouseleave', function() {SettingOverlay.style.display = 'none'; });
 
 const CloseLoot = document.getElementById('lootclose');
-const CloseResolve = document.getElementById('resolve_close');
 const CloseTorch = document.getElementById('torchlight_close');
-const DiseaseCloseBtn = document.getElementById("disease_close_btn"); 
 
-
-DiseaseCloseBtn.addEventListener('click', function() {
-  PlayPageClose()
-  DiseaseBox.style.display = 'none';
-});
 
 CloseLoot.addEventListener('click', function() {
   PlayPageClose()
   LootBox.style.display = 'none'
 })
 
-CloseResolve.addEventListener('click', function() {
-  PlayPageClose()
-  ResolveBox.style.display = 'none'
-})
+
 CloseTorch.addEventListener('click', function() {
   PlayPageClose()
   TorchBox.style.display = 'none'
